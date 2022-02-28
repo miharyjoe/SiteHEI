047e17d6ece68cfa75e27eac24f269f997f91525
Mihary Joel ANDRIAMILANTO <hei.mihary.2@gmail.com>
2022-02-28 20:02:10 +0300

    indexPhp for heroku

---
+++ index.php
@@ -0,0 +1,485 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="fr">
+  <!--<![endif]-->
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>HEI - Site Web</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/jquery.fancybox.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
+    />
+  </head>
+
+  <body>
+    <!-- Header Section -->
+    <div class="banner">
+      <!-- Navigation Section -->
+      <header id="header">
+        <div class="header-content clearfix">
+          <a class="logo" href="#">
+            <img src="images/logo.png" alt="logo" />
+          </a>
+          <nav class="navigation" role="navigation">
+            <ul class="primary-nav">
+              <li><a href="#introduction">Accueil</a></li>
+              <li><a href="#Formation">Formation</a></li>
+              <li><a href="#concour">Concours</a></li>
+              <li><a href="#contact">Contacts</a></li>
+            </ul>
+          </nav>
+          <a href="#" class="nav-toggle">Menu<span></span></a>
+        </div>
+      </header>
+      <!-- Navigation Section -->
+      <!-- Banner Section -->
+      <div class="container">
+        <div class="col-md-7 banner-inner-wrapper">
+          <div class="banner-text">
+            <h1>
+              Dr F. Lou MAURICA. Co-fondateur de HEI et Directeur pédagogique
+            </h1>
+            <p>
+              Professionnels du monde entier, <br />
+              unis pour former les étudiants malgaches suivant les plus hauts
+              standards.
+            </p>
+            <div class="text-center">
+              <a href="#Formation" class="btn">En savoir plus</a>
+
+            </div>
+          </div>
+          <!-- banner text -->
+        </div>
+      </div>
+      <!-- Banner Section -->
+    </div>
+    <!-- Header Section -->
+    <!-- Intro Section -->
+    <section id="introduction" class="section introduction">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+            <div class="intro-content">
+              <h4 class="text-center">Haute Ecole d'Informatique</h4>
+            </div>
+          </div>
+          <div class="col-md-5 col-sm-6">
+            <div class="intro-content">
+              <p class="text-justify">
+                L'informatique est l'un des domaines les plus rechercher à
+                Madagascar. Même des sociétés étrangers embauchent des informaticiens
+                venant d'ici. L'Université HEI a pour objectif de nous faire bénificier
+                de cette opportunité.
+              </p>
+              <p class="text-justify">
+                Nous avons des professeurs ayant des expériences internationales, 
+                 ainsi que des équipements aux normes internationales. On attend plus que
+                vous.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="intro-content">
+              <h4 class="text-center">Notre programme</h4>
+              <ul>
+                <li>- 2h de cours en présentiel</li>
+                <li>- 1.600.000 Ar écolage annuel en premiere année</li>
+                <li>
+                  - 0 AR écolage à partir de la deuxième année (si accepté en
+                  alternance)
+                </li>
+                <li>- 8.900 Euros, bourse d'excellence pour un Master à Paris</li>
+                <li>- Cours enregistrés afin d'apprendre chez soi</li>
+                <li>- Alternance possible aprés 1an</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Intro Section -->
+
+    <!-- work section -->
+
+    <section class="container-fluid" id="Formation">
+      <div class="jumbotron"  >
+        <h4 class="text-center">
+          Organisation des Unités d’Enseignements (UE)
+        </h4>
+        <p class="lead">
+          HEI délivre trois diplômes. Du moins élevé au plus élevé, ce sont : le
+          Brevet de Technicien (BT), le Diplôme Universitaire Technologique
+          (DUT), la Licence. La Licence est prévue être accréditée par l’Etat
+          malgache.
+        </p>
+        <hr class="my-4" />
+        <p class="text-justify">
+          HEI propose deux parcours : Ecosystèmes Logiciels (EL) et
+          Transformations Numériques (TN).<br />
+          Le parcours EL produit des profils capables de programmer les briques
+          de systèmes informatiques complexe. Il mène aux débouchés suivants :
+          développeur backend, développeur frontend, développeur mobile,
+          analyste de données, chef de projet, consultant en innovation. <br />
+          Le parcours TN produit des profils capable d’exploiter les briques
+          déjà programmées de systèmes informatiques complexes. Il mène aux
+          débouchés suivants : référenceur web, gestionnaire web,
+          commerçant-marketeur digital, analyste de données, devOps, chef de
+          projet, consultant en innovation.
+        </p>
+        <div class="text-center">
+          <a
+            class="btn"
+            href="assets/parcours.pdf"
+            role="button"
+            target="_blank"
+            >Plus d'information
+          </a>
+        </div>
+      </div>
+
+      <div class="jumbotron">
+        <h4 class="text-center">La bourse Vatsy & Les 5 d'HEI</h4>
+        <p class="lead">8.9k€ pour le/la major de promotion</p>
+        <hr class="my-4" />
+        <p class="text-justify">
+          Vatsy est un financement étudiant à remboursement retardé, pour
+          préparer un diplôme en France. Grâce à HEI, un financement sans
+          remboursement exigé est offert au plus méritant, ou à la plus
+          méritante. <br />
+          Par ailleurs, les 3 étudiant·e·s les mieux classé·e·s au concours
+          d'entrée se verront exonéré·e·s des frais de scolarité. Il en va de
+          même pour 2 étudiant·e·s supplémentaires évalué·e·s sur critères
+          sociaux.
+        </p>
+      </div>
+
+
+      <div class="jumbotron">
+        <h4 class="text-center">L'Alliance Française</h4>
+        <hr class="my-4" />
+        <p class="text-center">
+          La communication est aussi importante que la technique :
+          <a href="http://www.alliancefr.mg" target="_blank"
+            >l'Alliance Française</a
+          >
+          assure les cours de français d'HEI.
+        </p>
+      </div>
+    </section>
+
+
+    <div class="container ">
+      <h4 class="text-center text-capitalize my-5">
+        Un aperçu de l'équipe pédagogique
+      </h4>
+    </div>
+    <section id="works" class="works section no-padding">
+      <div class="container-fluid">
+        <div class="row no-gutter">
+          <div class="col-lg-2 col-md-4 col-sm-4 work">
+            <a href="images/Toky.png" class="work-box">
+              <img src="images/Toky.png" alt="" class="rounded-image" />
+              <div class="overlay">
+                <div class="overlay-caption">
+                  <p><i class="fa fa-search-plus fa-2x"></i></p>
+                  <h5>Toky<br /></h5>
+                  <h6>
+                    Enseignant HEI & EMIT, Doctorant en Systémes Multi-Agents
+                  </h6>
+                </div>
+              </div>
+              <!-- overlay -->
+            </a>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-4 work">
+            <a href="images/Harry.png" class="work-box">
+              <img src="images/Harry.png" alt="" />
+              <div class="overlay">
+                <div class="overlay-caption">
+                  <p><i class="fa fa-search-plus fa-2x"></i></p>
+                  <h5>Harry<br /></h5>
+                  <h6>Chef de projet digitale chez Baobab Group</h6>
+                </div>
+              </div>
+              <!-- overlay -->
+            </a>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-4 work">
+            <a href="images/Ryan.png" class="work-box">
+              <img src="images/Ryan.png" alt="" />
+              <div class="overlay">
+                <div class="overlay-caption">
+                  <p><i class="fa fa-search-plus fa-2x"></i></p>
+                  <h5>Ryan<br /></h5>
+                  <h6>Responsable Administratif HEI</h6>
+                </div>
+              </div>
+              <!-- overlay -->
+            </a>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-4 work">
+            <a href="images/Sandrine.png" class="work-box">
+              <img src="images/Sandrine.png" alt="" />
+              <div class="overlay">
+                <div class="overlay-caption">
+                  <p><i class="fa fa-search-plus fa-2x"></i></p>
+                  <h5>Sandrine <br /></h5>
+                  <h6>
+                    Précédente directrice technique de Vanilla Pay, Antananarivo
+                    Révélation Fintech de Women In Africa, 2020
+                  </h6>
+                </div>
+              </div>
+              <!-- overlay -->
+            </a>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-4 work">
+            <a href="images/Parison.png" class="work-box">
+              <img src="images/Parison.png" alt="" />
+              <div class="overlay">
+                <div class="overlay-caption">
+                  <p><i class="fa fa-search-plus fa-2x"></i></p>
+                  <h5>Parison<br /></h5>
+                  <h6>Ingénieur Google Londre</h6>
+                </div>
+              </div>
+              <!-- overlay -->
+            </a>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-4 work">
+            <a href="images/Tahina.png" class="work-box">
+              <img src="images/Tahina.png" alt="" />
+              <div class="overlay">
+                <div class="overlay-caption">
+                  <p><i class="fa fa-search-plus fa-2x"></i></p>
+                  <h5>Tahina<br /></h5>
+                  <h6>
+                    Docteure-Ingénieure CEA, Saclay Lauréate du prix
+                    UNESCO-L'Oréal pour les femmes en science, 2017
+                  </h6>
+                </div>
+              </div>
+              <!-- overlay -->
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- work section -->
+
+    <!-- Concours -->
+    <section class="container " id="concour">
+      <div class="my-5">
+        <h4 class="text-center">Inscription au concours d'entrée L1</h4>
+        <p class="text-center">
+          Téléchargez
+          <a href="assets/fiche.pdf" target="_blank"
+            >la fiche de renseignement</a
+          ><br />
+          Payez 40 000 Ar par MVola au 034 94 041 16 (Fihariantsoa) et notez sa
+          référence Remplissez les champs ci-dessous
+        </p>
+      </div>
+      
+      <form>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Prénom(s)" required/>
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Nom de famille"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control"  placeholder="E-mail" required />
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Téléphone" required/>
+        </div>
+        <div class="form-group">
+          <button
+            class="btn btn-block"
+            onclick="document.getElementById('getFile3').click()"
+          >
+            CIN ou Acte de naissance
+            <span
+              ><img
+                src="node_modules/bootstrap-icons/icons/plus-lg.svg"
+                alt="plus"
+            /></span>
+          </button>
+          <input
+            class="btn btn-block"
+            id="getFile3"
+            type="file"
+            style="display: none"
+          />
+        </div>
+        <div class="form-group">
+          <button
+            class="btn btn-block"
+            onclick="document.getElementById('getFile2').click()"
+          >
+            Photo d'identité récente
+            <span
+              ><img
+                src="node_modules/bootstrap-icons/icons/plus-lg.svg"
+                alt="plus"
+            /></span>
+          </button>
+          <input
+            class="btn btn-block"
+            id="getFile2"
+            type="file"
+            style="display: none"
+          />
+        </div>
+
+        <div class="form-group">
+          <button
+            class="btn btn-block"
+            onclick="document.getElementById('getFile1').click()"
+          >
+            Fiche de renseignement remplie<span
+              ><img
+                src="node_modules/bootstrap-icons/icons/plus-lg.svg"
+                alt="plus"
+            /></span>
+          </button>
+          <input
+            class="btn btn-block"
+            id="getFile1"
+            type="file"
+            style="display: none"
+          />
+        </div>
+
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="N° Référence MVola"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <h5>Date de concours souhaitée</h5>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="radio"
+                name="newsletter"
+                value="true"
+                class="form-check-input"
+              />17 Decembre 2021
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label">
+              <input
+                type="radio"
+                name="newsletter"
+                value="false"
+                checked
+                class="form-check-input"
+              />18 Decembre 2021
+            </label>
+          </div>
+        </div>
+        <div class="form-group">
+          <!--<input type="file" accept="image/png, image/Jpeg">-->
+          <button
+            class="btn btn-block"
+            onclick="document.getElementById('getFile').click()"
+          >
+            Bulletins de la Terminale ou Bac<span
+              ><img
+                src="node_modules/bootstrap-icons/icons/plus-lg.svg"
+                alt="plus"
+            /></span>
+          </button>
+          <input
+            class="btn btn-block"
+            id="getFile"
+            type="file"
+            style="display: none"
+          />
+        </div>
+
+        <div class="form-group text-center">
+          <input type="submit" value="S'inscrire" class="btn" />
+        </div>
+      </form>
+    </section>
+    
+
+    <!-- Concours -->
+
+    <!-- footer section -->
+    <footer id="contact" class="footer" >
+      <section class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h4>Contacts</h4>
+            <p>
+              Ivandry 101 Antananarivo, Madagascar.<br />
+              Call: +261 34 94 041 16 <br />
+              Email : <a href="mailto:contact@hei.school"> contact@hei.school </a>
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h4>Social presense</h4>
+            <ul class="footer-share">
+              <li>
+                <a href="https://web.facebook.com/hei.school/?ref=page_internal"
+                  target="_blank"><i class="fa fa-facebook"></i
+                ></a>
+              </li>
+            </ul>
+          </div>
+  
+          <div class="col-md-4">
+            <p>
+              © 2022 All rights reserved. All Rights Reserved<br />
+              Made with <i class="fa fa-heart pulse"></i> by
+              <a href="https://web.facebook.com/akira.kurusaki.9/" target="_blank">MiharyJoel</a>
+            </p>
+          </div>
+        
+        </div>
+        </section>
+      
+      <div id="retour">
+        <a href="#"
+          ><img
+            id="fleche"
+            src="node_modules/bootstrap-icons/icons/arrow-up-square-fill.svg"
+            alt="fleche"
+        /></a>
+      </div>
+    </footer>
+    <!-- footer section -->
+    <!-- JS FILES -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/retina.min.js"></script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
